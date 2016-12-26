0cec3ebc03f2271d31a0fe942178db6d719f98d0
Li Chien <lichien2010@yahoo.co.uk>
2016-12-26 20:42:51 +0000

    show data from MongoLab

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
+
+var dbuser = process.env.DB_USERNAME;
+var dbpassword = process.env.DB_PASS;
 var db;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-var dbuser = process.env.DB_USERNAME;
-var dbpassword = process.env.DB_PASS;
 MongoClient.connect("mongodb://" + dbuser + ":" + dbpassword + "@ds141368.mlab.com:41368/retrospectives", function (err, database) {
   db = database;
   app.listen(3000, function () {
@@ -17,8 +18,13 @@ MongoClient.connect("mongodb://" + dbuser + ":" + dbpassword + "@ds141368.mlab.c
   });
 });
 
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  var retrosData = db.collection('retros').find().toArray(function (err, results) {
+    if (err) return console.log(err);
+    res.render('index.ejs', { retros: results });
+  });
 });
 
 app.post('/retros', function (req, res) {
