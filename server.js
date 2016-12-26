ddc73331ae7fed37887e8a536df0c327c9f89643
Li Chien <lichien2010@yahoo.co.uk>
2016-12-26 20:25:09 +0000

    connect to MongoLab db and add env file

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+
+require('dotenv').config();
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var db;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
-})
+var dbuser = process.env.DB_USERNAME;
+var dbpassword = process.env.DB_PASS;
+MongoClient.connect("mongodb://" + dbuser + ":" + dbpassword + "@ds141368.mlab.com:41368/retrospectives", function (err, database) {
+  db = database;
+  app.listen(3000, function () {
+    console.log("listening on port 3000");
+  });
+});
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html");
-})
+});
 
-app.post('/retros', (req, res) => {
-  console.log(req.body);
-})
+app.post('/retros', function (req, res) {
+  db.collection('retros').save(req.body, function (err, result) {
+    if (err) return console.log(err);
+    console.log('saved to database');
+    res.redirect('/');
+  });
+});
